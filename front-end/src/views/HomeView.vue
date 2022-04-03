77ac283b18281a56cb63ff06db74627b35289b35
folskeat <keatonfolsom@gmail.com>
2022-04-03 12:10:37 -0600

    initial commit

---
+++ front-end/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="cups">
+      <img src="data/courses/cups/mushroom_icon.png">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cups {
+  display: flex;
+}
+</style>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
