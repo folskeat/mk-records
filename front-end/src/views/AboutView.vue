ad3f570865c0ed57e8142ed8f0799359e4e3a227
folskeat <keatonfolsom@gmail.com>
2022-04-06 10:40:43 -0600

    disclaimer

--- front-end/src/views/AboutView.vue
+++ front-end/src/views/AboutView.vue
@@ -3,5 +3,8 @@
     <h1>About us!</h1>
     <p>Upload your best records from time trials!</p>
     <p>We automatically sort racers by whoever is fastest!</p>
+    <br />
+    <p>*All names, images, sounds, and music are a property of Nintendo.</p>
+    <p>This is a fan project for non-commercial use.</p>
   </div>
 </template>
